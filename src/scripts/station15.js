519477b19288de5b7bbe8cbe296a734f60c34469
shtk0llq <shtk0llq@gmail.com>
2024-08-21 05:02:36 +0900

    station 15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,22 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
-}
+function test(users) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        const newUsers = users.map((user) => {
+          return { ...user, full_name: `${user.family_name} ${user.first_name}` };
+        });
 
+        resolve(newUsers);
+      } catch (error) {
+        reject(error);
+      }
+    }, 3000);
+  });
+}
